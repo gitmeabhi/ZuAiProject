f3189bd255ca0d7cdad3b1c02ef272d1e5d4340d
abraham <aabhi9755@gmail.com>
2024-08-25 12:52:49 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mysql = require("mysql")
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 const db = mysql.createConnection({
@@ -12,12 +13,58 @@ const db = mysql.createConnection({
     database : "test"
 })
 
-app.get("/", (req, res) =>{
-   const sql = "SELECT * FROM students"
-   db.query(sql, (err, data) =>{
+app.get("/posts", (req, res) =>{
+    const sql = "SELECT * FROM posts"
+    db.query(sql, (err, data) =>{
+     if(err) return res.json(err)
+     return res.json(data)
+    })
+ })
+
+app.get("/posts/:id", (req, res) =>{
+    
+   const sql = "SELECT * FROM posts WHERE id = ? "
+   const id =req.params.id
+   db.query(sql,[id], (err, data) =>{
     if(err) return res.json(err)
     return res.json(data)
    })
+})
+
+app.post("/posts", (req, res) =>{
+    const sql = "INSERT INTO posts(`title`, `description`, `imgaeUrl`) VALUES(?)"
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.imgaeUrl
+    ]
+    db.query(sql, [values], (err, data) =>{
+        if(err) return res.json("Error")
+        return res.json(data)
+    })
+})
+
+app.put("/update/:id", (req, res) =>{
+    const sql = "UPDATE posts SET `title` = ?, `description` = ? `imgaeUrl` = ? WHERE id = ?"
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.imgaeUrl
+    ]
+    const id = req.params.id
+    db.query(sql, [...values, id], (err, data) =>{
+        if(err) return res.json("Error")
+        return res.json(data)
+    })
+})
+
+app.delete("/posts/:id", (req,res) =>{
+    const sql = "DELETE FROM posts WHERE id = ?"
+    const id = req.params.id
+    db.query(sql,[id],(err, data) =>{
+        if(err) return res.json("Error")
+        return res.json(data)
+    })
 })
 
 app.listen(3001, () =>{
